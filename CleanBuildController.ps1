3142cd37e19025dc89779f574d75a9d5c40571e7
alexnrod <32619434+alexnrod@users.noreply.github.com>
2019-01-14 15:01:53 +0300

    Add files via upload

---
+++ CleanBuildController.ps1
@@ -0,0 +1,33 @@
+# Variables
+# Delete the buld Artifacts older than:
+$DateForCompare1 = (Get-Date).AddDays(-7)
+$DateForCompare2 = (Get-Date).AddDays(-14)
+$ServiceUserName = "_tfsservice"
+
+# I divided the deletion for the Deployment and Result folders only for the example
+# Possibly, will be enough to delete all build artifacts older than (n days)
+
+# Delete Test run Deployment (older then 1 Week) folders exclude Results folders
+
+$PathToDelete = "C:\Users\$ServiceUserName\AppData\Local\VSEQT\QTController"
+$ExcludeFolder = "Results"
+
+$targets = @(Get-ChildItem $PathToDelete -Exclude $ExcludeFolder | Where {$_.psIsContainer -eq $true -and $_.CreationTime -lt $DateForCompare1})
+
+if ($targets.Count -ne 0)
+{
+	"Deleting:"
+	$targets
+	$targets | Remove-Item -Recurse -Force
+}
+
+# Delete test run Results folders older than 2 Weeks
+
+$targets = @(Get-ChildItem $PathToDelete | Where {$_.psIsContainer -eq $true -and $_.CreationTime -lt $DateForCompare2})
+
+if ($targets.Count -ne 0)
+{
+	"Deleting:"
+	$targets
+	$targets | Remove-Item -Recurse -Force
+}
