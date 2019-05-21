0601c747bcd7081f770c454f2914e9baf673baa0
alexnrod <32619434+alexnrod@users.noreply.github.com>
2019-05-21 20:17:05 +0300

    Pasted a code from another GitHub project

--- CleanBuildController.ps1
+++ CleanBuildController.ps1
@@ -1,32 +1,14 @@
-# Variables
-# Delete the buld Artifacts older than:
-$DateForCompare1 = (Get-Date).AddDays(-137)
-$ServiceUserName = "_tfsservice"
-
-# I divided the deletion for the Deployment and Result folders only for the example
-# Possibly, will be enough to delete all build artifacts older than (n days)
-
-# Delete Test run Deployment (older then 1 Week) folders exclude Results folders
-
-$PathToDelete = "C:\Users\$ServiceUserName\AppData\Local\VSEQT\QTController"
-$ExcludeFolder = "Results"
-
-$targets = @(Get-ChildItem $PathToDelete -Exclude $ExcludeFolder | Where {$_.psIsContainer -eq $true -and $_.CreationTime -lt $DateForCompare1})
-
-if ($targets.Count -ne 0)
-{
-	"Deleting:"
-	$targets
-	$targets | Remove-Item -Recurse -Force
-}
-
-# Delete test run Results folders older than 2 Weeks
-
-$targets = @(Get-ChildItem $PathToDelete | Where {$_.psIsContainer -eq $true -and $_.CreationTime -lt $DateForCompare2})
-
-if ($targets.Count -ne 0)
-{
-	"Deleting:"
-	$targets
-	$targets | Remove-Item -Recurse -Force
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Conditionally included if framing is not allowed
+ */
+if (self === top) {
+    var style_element = document.getElementById('cfs-style');
+    // check if style_element has already been removed
+    // to avoid frequently reported js error
+    if (typeof(style_element) !== 'undefined' && style_element !== null) {
+        style_element.parentNode.removeChild(style_element);
+    }
+} else {
+    top.location = self.location;
 }
